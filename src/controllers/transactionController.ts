3e58a3dd1a9c126992e350398040ac1f295acdb0
Rio Pahlevy <43176811+rpahlevy@users.noreply.github.com>
2024-08-07 15:25:26 +0700

    refactor

---
+++ src/controllers/transactionController.ts
@@ -0,0 +1,28 @@
+import { TRANSACTION_TYPE } from '@prisma/client';
+import { FastifyRequest, FastifyReply } from 'fastify';
+import { handleTransaction } from '../services/transactionService';
+import { TransactionRequest } from '../types/requests';
+
+export const send = async (request: FastifyRequest, reply: FastifyReply) => {
+  try {
+    const { sender_account_number, receiver_account_number, amount, currency } = request.body as TransactionRequest;
+    const user_id = request.user.id;
+
+    const transaction = await handleTransaction(user_id, sender_account_number, receiver_account_number, amount, currency, TRANSACTION_TYPE.SEND);
+    reply.send(transaction);
+  } catch (error: any) {
+    reply.status(400).send({ error: error.message });
+  }
+};
+
+export const withdraw = async (request: FastifyRequest, reply: FastifyReply) => {
+  try {
+    const { sender_account_number, amount, currency } = request.body as TransactionRequest;
+    const user_id = request.user.id;
+
+    const transaction = await handleTransaction(user_id, sender_account_number, null, amount, currency, TRANSACTION_TYPE.WITHDRAW);
+    reply.send(transaction);
+  } catch (error: any) {
+    reply.status(400).send({ error: error.message });
+  }
+};
