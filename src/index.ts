e92d8cc923d48c18ee477d1c9ea635e412cf546b
Rio Pahlevy <43176811+rpahlevy@users.noreply.github.com>
2024-08-08 01:00:19 +0700

    add swagger

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,9 @@ import authRoutes from './routes/authRoute';
 import paymentAccountRoutes from './routes/paymentAccountRoute';
 import paymentHistoryRoutes from './routes/paymentHistoryRoute';
 import transactionRoutes from './routes/transactionRoute';
+import fastifyFormbody from '@fastify/formbody';
+import fastifySwagger from '@fastify/swagger';
+import fastifySwaggerUi from '@fastify/swagger-ui';
 
 const app = Fastify({ logger: true });
 
@@ -13,6 +16,51 @@ app.register(fastifyPlugin(async (fastify: FastifyInstance) => {
   fastify.decorate('prisma', prisma);
   fastify.decorate('supabase', supabase);
 }));
+
+// swagger
+app.register(fastifyFormbody);
+app.register(fastifySwagger, {
+  swagger: {
+    info: {
+      title: 'BEASGN - BE Assignment',
+      description: 'A backend service project implementing an Account Manager and Payment Manager using Node.js, Fastify, and Prisma. This project includes user registration, login, account management, and transaction processing, with Docker setup and Swagger documentation for API endpoints.',
+      version: '1.0.0',
+    },
+    host: 'localhost:3000',
+    schemes: ['http'],
+    consumes: ['application/json'],
+    produces: ['application/json'],
+    securityDefinitions: {
+      bearerAuth: {
+        type: 'apiKey',
+        name: 'Authorization',
+        in: 'header',
+        description: 'Enter JWT Bearer token **_with prefix_**, e.g "Bearer asdfg123"',
+      }
+    },
+  },
+});
+app.register(fastifySwaggerUi, {
+  routePrefix: '/docs',
+  uiConfig: {
+    // docExpansion: 'full',
+    deepLinking: false,
+  },
+  uiHooks: {
+    onRequest: function (request, reply, next) {
+      next();
+    },
+    preHandler: function (request, reply, next) {
+      next();
+    },
+  },
+  staticCSP: true,
+  transformStaticCSP: (header) => header,
+  transformSpecification: (swaggerObject, request, reply) => {
+    return swaggerObject;
+  },
+  transformSpecificationClone: true,
+});
 
 // Register routes
 app.register(authRoutes);
